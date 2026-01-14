aa91a4b4777c20da8133b348d2d8b05fd8d76ae7
Lay <dogfish520@gmail.com>
2026-01-14 16:39:08 +0800

    sok py minor bugfix

--- Sok/Python/Polymorphism/Task1.py
+++ Sok/Python/Polymorphism/Task1.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 class ChessPiece(ABC):
     @abstractmethod
-    def __init__(self, horizontal, vertical):
+    def __init__(self, vertical, horizontal):
         self.horizontal = horizontal
         self.vertical = vertical
     @abstractmethod
@@ -10,31 +10,31 @@ class ChessPiece(ABC):
         pass
 
 class King(ChessPiece):
-    def __init__(self, horizontal, vertical):
+    def __init__(self, vertical, horizontal):
         self.horizontal = horizontal
         self.vertical = vertical
 
-    def can_move(self, horizontal, vertical):
+    def can_move(self, vertical, horizontal):
         if abs(self.horizontal - horizontal)<=1 and abs(ord(self.vertical) - ord(vertical))<=1:
             return "Can"
         else:
             return "Can't"
 
 class Knight(ChessPiece):
-    def __init__(self, horizontal, vertical):
+    def __init__(self, vertical, horizontal):
         self.horizontal = horizontal
         self.vertical = vertical
 
-    def can_move(self, horizontal, vertical):
+    def can_move(self, vertical, horizontal):
         if (abs(self.horizontal - horizontal)==2 and abs(ord(self.vertical) - ord(vertical))==1) or (abs(self.horizontal - horizontal)==1 and abs(ord(self.vertical) - ord(vertical))==2):
             return "Can"
         else:
             return "Can't"
 
-lst = [King(5, 'b'), Knight(4, 'c')]
+lst = [King('b', 5), Knight('c', 4)]
 
 for i in lst:
-    print(i.can_move(5, 'c'))
-    print(i.can_move(2, 'd'))
-    print(i.can_move(5, 'e'))
-    print(i.can_move(4, 'd'))
+    print(i.can_move('c', 5))
+    print(i.can_move('d', 2))
+    print(i.can_move('e', 5))
+    print(i.can_move('d', 4))
